4186889f561ca35fb4ae947ff9a26b2950095427
Nikhil Sharma <95125154+nikhil-bhardwaj21@users.noreply.github.com>
2024-01-08 20:02:50 +0530

    assigment

---
+++ Assigmnet/script.js
@@ -0,0 +1,7 @@
+// You can add any additional JavaScript functionality here if needed
+document.getElementById('contact-form').addEventListener('submit', function (event) {
+    // Add your form submission logic here
+
+    // Prevent the default form submission
+    event.preventDefault();
+});
